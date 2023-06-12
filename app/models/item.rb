df98c6cd5e4cbb24185ee50bca06fafc91f00925
umaumau <mako.melon@gmail.com>
2023-06-12 22:50:36 +0900

    出品機能

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
+  validates :image        , presence: true
   validates :name         , presence: true
   validates :content      , presence: true
   validates :category_id  , numericality: { other_than: 1 , message: "can't be blank"} 
