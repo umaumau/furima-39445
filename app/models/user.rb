0100ca86a7a7596b508fe3c381db20e1321c775c
umaumau <mako.melon@gmail.com>
2023-06-12 12:13:07 +0900

    ユーザー管理機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  
+  validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: "は半角英数字を含めてください" }
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :last_name, presence: true, format: { with: /\p{Han}|\p{Hiragana}|\p{Katakana}/, message: "は全角で入力してください"}
+  validates :first_name, presence: true, format: { with: /\p{Han}|\p{Hiragana}|\p{Katakana}/, message: "は全角で入力してください"}
+  validates :last_name_kana, presence: true, format: { with: /\p{Katakana}/, message: "は全角カナで入力してください"}
+  validates :first_name_kana, presence: true, format: { with: /\p{Katakana}/, message: "は全角カナで入力してください"}
   validates :birth, presence: true
 
   has_many :items
