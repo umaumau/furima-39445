471d5314deeaceaec4ea183015330c071febf7ef
umaumau <mako.melon@gmail.com>
2023-06-12 19:29:32 +0900

    Itemモデルの作成

---
+++ app/models/item.rb
@@ -0,0 +1,12 @@
+class Item < ApplicationRecord
+  validates :name         , presence: true
+  validates :content      , presence: true
+  validates :category_id  , numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :condition_id , numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :postage_id   , numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :duration_id  , numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :price        , presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }, format: { with: /\A\d+\z/ }
+
+  has_one_attached :image
+end
