e865194e5682b1610d0ef2df1e4465d19bf03979
umaumau <mako.melon@gmail.com>
2023-06-12 15:25:39 +0900

    Userモデルのバリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: "は半角英数字を含めてください" }
+  validates :password, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?[\d])[a-zA-Z\d]+\z/, message: "は半角英数字を含めてください" }
   validates :nickname, presence: true
-  validates :last_name, presence: true, format: { with: /[一-龠々]|[ぁ-ん]|[ァ-ヴ]/, message: "は全角で入力してください"}
-  validates :first_name, presence: true, format: { with: /[一-龠々]|[ぁ-ん]|[ァ-ヴ]/, message: "は全角で入力してください"}
-  validates :last_name_kana, presence: true, format: { with: /[ァ-ヴ]/, message: "は全角カナで入力してください"}
-  validates :first_name_kana, presence: true, format: { with: /[ァ-ヴ]/, message: "は全角カナで入力してください"}
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角カナを使用してください' }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角カナを使用してください' }
   validates :birth, presence: true
 
   # has_many :items
