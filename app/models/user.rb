4969445147a144c3c4522ccf5dae18d4d4af0085
umaumau <mako.melon@gmail.com>
2023-06-12 14:47:01 +0900

    ユーザー管理機能の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,13 @@ class User < ApplicationRecord
   
   validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: "は半角英数字を含めてください" }
   validates :nickname, presence: true
-  validates :last_name, presence: true, format: { with: /\p{Han}|\p{Hiragana}|\p{Katakana}/, message: "は全角で入力してください"}
-  validates :first_name, presence: true, format: { with: /\p{Han}|\p{Hiragana}|\p{Katakana}/, message: "は全角で入力してください"}
-  validates :last_name_kana, presence: true, format: { with: /\p{Katakana}/, message: "は全角カナで入力してください"}
-  validates :first_name_kana, presence: true, format: { with: /\p{Katakana}/, message: "は全角カナで入力してください"}
+  validates :last_name, presence: true, format: { with: /[一-龠々]|[ぁ-ん]|[ァ-ヴ]/, message: "は全角で入力してください"}
+  validates :first_name, presence: true, format: { with: /[一-龠々]|[ぁ-ん]|[ァ-ヴ]/, message: "は全角で入力してください"}
+  validates :last_name_kana, presence: true, format: { with: /[ァ-ヴ]/, message: "は全角カナで入力してください"}
+  validates :first_name_kana, presence: true, format: { with: /[ァ-ヴ]/, message: "は全角カナで入力してください"}
   validates :birth, presence: true
 
-  has_many :items
-  has_many :orders
+  # has_many :items
+  # has_many :orders
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,12 +15,12 @@ ActiveRecord::Schema.define(version: 2023_06_09_125516) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "nickname"
-    t.string "last_name"
-    t.string "first_name"
-    t.string "last_name_kana"
-    t.string "first_name_kana"
-    t.date "birth"
+    t.string "nickname", null: false
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "last_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.date "birth", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
