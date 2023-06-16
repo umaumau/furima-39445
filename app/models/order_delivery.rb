c0df701d200d4bd4f192779ccd11c746af35f5d5
umaumau <mako.melon@gmail.com>
2023-06-16 16:23:41 +0900

    修正依頼箇所の修正

--- app/models/order_delivery.rb
+++ app/models/order_delivery.rb
@@ -3,23 +3,15 @@ class OrderDelivery
   attr_accessor :item_id, :user_id, :postal_code, :prefecture_id, :city, :address, :building, :phone, :token
 
   with_options presence: true do
+    validates :token
     validates :item_id
     validates :user_id
-    validates :postal_code  , presence: true
-  end
-  
-  validates :postal_code  , format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
-  validates :prefecture_id, numericality: {other_than: 1 , message: "can't be blank"} 
-
-  with_options presence: true do
+    validates :postal_code  , format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
+    validates :prefecture_id, numericality: {other_than: 1 , message: "can't be blank"} 
     validates :city
     validates :address
-    validates :phone
+    validates :phone        , length: {minimum: 10, maximum: 11, message: "is too short"}, numericality: {only_integer: true, message: "is invalid. Input only number"}
   end
-
-  validates :phone        , length: {minimum: 10, maximum: 11, message: "is too short"}
-  validates :phone        , numericality: {only_integer: true, message: "is invalid. Input only number"}
-  validates :token        , presence: true
 
   def save
     order = Order.create(item_id: item_id, user_id: user_id)
