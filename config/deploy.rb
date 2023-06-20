4c8ebbfc0892358bc45713441264e4bbcd24788d
umaumau <mako.melon@gmail.com>
2023-06-20 20:58:07 +0900

    ブランチ名の指定

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,6 +7,8 @@ set :application, 'furima-39445'
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:umaumau/furima-39445.git'
 
+set :branch, "main"
+
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
